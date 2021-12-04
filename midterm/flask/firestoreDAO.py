05ee10a65f67c86bea7b29272e6971037e1e3fe6
Ron <t107590012@ntut.org.tw>
2021-12-05 06:20:25 +0800

    doing firestoreDAO.py and editing main.py , haven't finished implementation

--- midterm/flask/firestoreDAO.py
+++ midterm/flask/firestoreDAO.py
@@ -13,25 +13,64 @@ class FirestoreDAO:
         return user.role
     
     #myFavorite
-    def getMyFavorites(self, userId):
-        products =[]
+    def getFavorites(self, userId):
         user_ref = self.collection('users').document(f'userId')
-        favorites = user_ref.collection(f'users/{userId}/favorites')
-        return favorites.get().to_dict()
+        favorites_col = user_ref.collection(f'users/{userId}/favorites')
+        return favorites_col.get().to_dict()
         
-    #search_result
+    #search_result / stockManagement
     def getProducts(self, userId, keyword=None):
         products = []
-        products_info = self.__db.collection('orders').where(keyword,'in','name').order_by('name').stream()
-        for info in products_info:
-            products.append(info)
+        if keyword : #search
+            products_col = self.__db.collection('orders').where(keyword,'in','name').order_by('name').stream()
+            products = products_col.to_dict()
+            #for info in products_col:
+            #    products.append(info)
+        elif self.getRole(userId) == 'admin':
+            products_col = self.__db.collection('orders').stream()
+            products = products_col.to_dict()
         return products
     
-    #orderRecord
+    #orderRecord, orderManagement
     def getOrder(self, userId):
         orders = []
-        orders_info = self.__db.collection('orders').where('userId','==',userId).order_by('timestamp', direction=firestore.Query.DESCENDING).stream()
-        for info in orders_info:
-            orders.append(info._data)
+        if self.getRole() == 'admin':
+            orders_col = self.__db.collection('orders').stream()
+            orders = orders_col.to_dict()
+        elif self.getRole() == 'customer':
+            orders_col = self.__db.collection('orders').where('userId','==',userId).order_by('timestamp', direction=firestore.Query.DESCENDING).stream()
+            for info in orders_col:
+                orders.append(info._data)
         return orders
     
+    #
+    def setFavorites(self, userId, productInfo):
+        user_ref = self.__db.collection('users').document(f'{userId}')
+        favorites_col = user_ref.collection('favorites')
+        favorites_col.set(productInfo)
+    
+    #
+    def deleteFavorites(self, userId, productId):
+        favorite_ref = self.__db.collection('users/{userId}/favorites')
+        product_doc = favorite_ref.document('productId')
+        product_doc.delete()
+        
+    #
+    def addOrder(self, userId, orderInfo):
+        orderInfo['userId'] = userId
+        self.__db.collection('orders').add(orderInfo)
+    #orderManagement
+    def updateOrder(self, orderInfo):
+        orderId = orderInfo['orderId']
+        order_doc = self.__db.collection('orders').document(f'{orderId}')
+        order_doc.set(orderInfo)
+    
+    #stockManagement
+    def addProduct(self, productInfo):
+        self.__db.collection('products').add(productInfo)
+    #stockManagement
+    def updateProduct(self, productInfo):
+        productId = productInfo['productId']
+        product_doc = self.__db.collection('products').document(f'{productId}')
+        product_doc.set(productInfo)
+
--- midterm/flask/main.py
+++ midterm/flask/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, request, redirect
-
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+import firestoreDAO
+import json
 app = Flask(__name__)
 
 
@@ -56,132 +57,62 @@ def search_result():
     return render_template("search_result.html", **locals())
 
 #both
-#@app.route('/myFavorite/<userId>', methods=['GET', 'POST'])
-@app.route('/myFavorite', methods=['GET', 'POST'])
-def myFavorite():
+#@app.route('/myFavorite/<userId>', methods=['GET'])
+@app.route('/myFavorite', methods=['GET'])
+def myFavorite(userId):
     title = "我的最愛"
-    response = {
-            "products":[
-                {
-                    "name":"3080",
-                    "brand":"ASUS",
-                    "price": 1000,
-                    "quantity": 5,
-                    "imgSrc": "https://cdn.vox-cdn.com/thumbor/Y8HSRGJGLdHmQlIkOFoA-jUtBzA=/0x0:2640x1749/1200x800/filters:focal(1109x664:1531x1086)/cdn.vox-cdn.com/uploads/chorus_image/image/69746324/twarren_rtx3080.0.jpg"
-                },
-                {
-                    "name":"3070",
-                    "brand":"ROG",
-                    "price": 800,
-                    "quantity": 0,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                }
-            ]
-    }
+    myFavorites = firestoreDAO.getFavorites(userId)
     return render_template("myFavorite.html", **locals())
+
+@app.route('setFavorite',methods=['POST'])
+def setFavorite(userId, productInfo):
+    #process productInfo
+    response = {
+        "id": productInfo["id"],
+        "name": productInfo["name"],
+        "brand": productInfo["brand"],
+        "price": productInfo["price"],
+        "quantity": productInfo["quantity"],
+    }
+    firestoreDAO.setFavorites(userId, response)
+    return jsonify('加入最愛成功')
+
 #buyer state:-1 處理中, 0 運送中 , 1已完成
 #@app.route('/orderRecord/<userId>', methods=['GET', 'POST'])
 @app.route('/orderRecord', methods=['GET', 'POST'])
-def orderRecord():
+def orderRecord(userId):
     title = "訂單紀錄"
-    response = {
-            "records":[
-                {
-                    "name":"3080",
-                    "brand":"ASUS",
-                    "price": 1000,
-                    "quantity": 5,
-                    "date": "11/27",
-                    "state": 0,
-                    "imgSrc": "https://cdn.vox-cdn.com/thumbor/Y8HSRGJGLdHmQlIkOFoA-jUtBzA=/0x0:2640x1749/1200x800/filters:focal(1109x664:1531x1086)/cdn.vox-cdn.com/uploads/chorus_image/image/69746324/twarren_rtx3080.0.jpg"
-                },
-                {
-                    "name":"3070Ti",
-                    "brand":"MSI",
-                    "price": 800,
-                    "quantity": 1,
-                    "date": "11/29",
-                    "state": -1,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                },
-                {
-                    "name":"3070",
-                    "brand":"ROG",
-                    "price": 800,
-                    "quantity": 1,
-                    "date": "12/01",
-                    "state": 1,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                }
-            ]
-    }
+    orders = firestoreDAO.getOrder(userId)
     return render_template("orderRecord.html", **locals())
+
+@app.route('/updateOrder', methods=['POST'])
+def updateOrder(userId, orderInfo):
+    title = "訂單紀錄"
+    orders = firestoreDAO.updateOrder(userId, orderInfo)
+    return jsonify('更新訂單成功')
+
+@app.route('/addOrder', methods=['POST'])
+def addOrder(userId, orderInfo):
+    title = "訂單紀錄"
+    orders = firestoreDAO.addOrder(userId, orderInfo)
+    return jsonify('新增訂單成功')
 
 
 #seller
 #@app.route('/stockManagement/<userId>', methods=['GET', 'POST'])
-@app.route('/stockManagement', methods=['GET', 'POST'])
-def stockManagement():
+@app.route('/stockManagement', methods=['GET'])
+def stockManagement(userId):
     title = "商品管理"
-    response = {
-            "products":[
-                {
-                    "name":"3080",
-                    "brand":"ASUS",
-                    "price": 1000,
-                    "quantity": 5,
-                    "onShelf" : 1,
-                    "imgSrc": "https://cdn.vox-cdn.com/thumbor/Y8HSRGJGLdHmQlIkOFoA-jUtBzA=/0x0:2640x1749/1200x800/filters:focal(1109x664:1531x1086)/cdn.vox-cdn.com/uploads/chorus_image/image/69746324/twarren_rtx3080.0.jpg"
-                },
-                {
-                    "name":"3070",
-                    "brand":"ROG",
-                    "price": 800,
-                    "quantity": 0,
-                    "onShelf" : 0,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                }
-            ]
-    }
+    produtcts = firestoreDAO.getProducts(userId)
     return render_template("stockManagement.html", **locals())
 
 
 #seller
 #@app.route('/orderManagement/<userId>', methods=['GET', 'POST'])
-@app.route('/orderManagement', methods=['GET', 'POST'])
-def orderManagement():
+@app.route('/orderManagement', methods=['GET'])
+def orderManagement(userId):
     title = "訂單管理"
-    response = {
-            "orders":[
-                {
-                    "name":"3080",
-                    "brand":"ASUS",
-                    "price": 1000,
-                    "quantity": 5,
-                    "date": "11/27",
-                    "state": 0,
-                    "imgSrc": "https://cdn.vox-cdn.com/thumbor/Y8HSRGJGLdHmQlIkOFoA-jUtBzA=/0x0:2640x1749/1200x800/filters:focal(1109x664:1531x1086)/cdn.vox-cdn.com/uploads/chorus_image/image/69746324/twarren_rtx3080.0.jpg"
-                },
-                {
-                    "name":"3070Ti",
-                    "brand":"MSI",
-                    "price": 800,
-                    "quantity": 1,
-                    "date": "11/29",
-                    "state": -1,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                },
-                {
-                    "name":"3070",
-                    "brand":"ROG",
-                    "price": 800,
-                    "quantity": 1,
-                    "date": "12/01",
-                    "state": 1,
-                    "imgSrc":"https://cf.shopee.tw/file/e999d155a61197595757fa4945c589f9"
-                }
-            ]
-    }
+    orders = firestoreDAO.getOrder(userId)
     return render_template("orderManagement.html", **locals())
 
 
