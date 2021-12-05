3228c22e4cacf0c367c5dea61884badb4402cd7c
guyleaf <ychhua1@gmail.com>
2021-12-05 11:03:35 +0800

    Solve count problem by nesting the products collections

--- setup/upload/firestoreDao.py
+++ setup/upload/firestoreDao.py
@@ -3,17 +3,34 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from typing import Any
 import datetime
+import random
 
 from settings import project_id
 
 
+class Shard:
+    """
+    A shard is a distributed counter. Each shard can support being incremented
+    once per second. Multiple shards are needed within a Counter to allow
+    more frequent incrementing.
+    """
+
+    def __init__(self):
+        self._count = 0
+
+    def to_dict(self):
+        return {"count": self._count}
+
+
 class FirestoreDao:
     _db: firestore.firestore.Client
+    _num_of_shards: int
 
     def __init__(self):
         cred = credentials.ApplicationDefault()
         firebase_admin.initialize_app(cred, {"project_id": project_id})
         self._db = firestore.client()
+        self._num_of_shards = 10
 
     def upload_code_data(self, data: list[dict[str, Any]]):
         batch_action = self._db.batch()
@@ -37,22 +54,44 @@ class FirestoreDao:
 
         return [document.to_dict() for document in documents]
 
-    def upload_products(self, products: list[dict[str, Any]]):
-        batch_action = self._db.batch()
-        products_collection = self._db.collection(u"products")
+    def _init_shards(self, collection):
+        for num in self._num_of_shards:
+            shard = Shard()
+            collection.document(str(num)).set(shard.to_dict())
 
-        shared_document = products_collection.document("shared")
-        batch_action.create(shared_document, {"count": 0})
-        sub_collection = shared_document.collection("items")
+    def upload_products(self, products: list[dict[str, Any]]):
+        products_collection = self._db.collection(u"products")
+        self._init_shards(products_collection)
+
+        batch_action = self._db.batch()
 
         for product in products:
             date_time = datetime.datetime.now()
             product["created_time"] = date_time
             product["updated_time"] = date_time
-            product_document = sub_collection.document(product["id"])
-            batch_action.create(product_document, product)
+
+            # products_collection: [ <=== products_document
+            #     {
+            #         count: int,
+            #         items_collection: [
+            #             { <=== items_document
+            #
+            #             }
+            #         ]
+            #     }
+            # ]
+
+            # pick one document randomly
+            # similar to get hashed value and select bucket
+            document_id = random.randint(0, self._num_shards - 1)
+            products_document = products_collection.document(str(document_id))
+
+            items_collection = products_document.collection("items")
+            items_document = items_collection.document(product["id"])
+            batch_action.create(items_document, product)
+
             batch_action.update(
-                shared_document, {"count": firestore.firestore.Increment(1)}
+                products_document, {"count": firestore.firestore.Increment(1)}
             )
 
         batch_action.commit()
