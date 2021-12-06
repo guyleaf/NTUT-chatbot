37100d8b8499b4cea43ffdd1c22912426d1daad0
guyleaf <ychhua1@gmail.com>
2021-12-06 08:53:39 +0800

    Working on myFavorites function

---
+++ midterm/flask/static/js/favorite.js
@@ -0,0 +1,43 @@
+﻿function setupFavoriteFunction(userId) {
+    let addFavoriteModal = new bootstrap.Modal(
+        document.getElementById("addFavoriteSuccessHint")
+    );
+    let deleteFavoriteModal = new bootstrap.Modal(
+        document.getElementById("deleteFavoriteSuccessHint")
+    );
+    $(".deleteFavoriteButton").click(function (e) {
+        e.preventDefault();
+        const productId = $(e.target).data("product-id");
+        $.ajax({
+            url: userId + "/myFavorites",
+            type: "DELETE",
+            data: JSON.stringify({ product_id: productId }),
+        })
+            .done(function (_) {
+                console.log("success");
+                $(e.target).hide();
+                $(e.target).siblings(".addFavoriteButton").show();
+                deleteFavoriteModal.show();
+            })
+            .fail(function (error) {
+                console.error(error);
+            });
+    });
+    $(".addFavoriteButton").click(function (e) {
+        e.preventDefault();
+        const productId = $(e.target).data("product-id");
+        $.post(
+            userId + "/myFavorites",
+            JSON.stringify({ product_id: productId })
+        )
+            .done(function (_) {
+                console.log("success");
+                $(e.target).hide();
+                $(e.target).siblings(".deleteFavoriteButton").show();
+                addFavoriteModal.show();
+            })
+            .fail(function (error) {
+                console.error(error);
+            });
+    });
+}
